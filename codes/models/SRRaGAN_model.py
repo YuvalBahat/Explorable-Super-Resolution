Small bug and adding D verification by looking at average of correctly distinguished images in each previous step.

codes/models/SRRaGAN_model.py
@@ -44,7 +44,7 @@ class SRRaGANModel(BaseModel):
                 self.DTE_net.WrapArchitecture_PyTorch(only_padders=True)
         self.netG = networks.define_G(opt,DTE=self.DTE_net).to(self.device)  # G
         logs_2_keep = ['l_g_pix', 'l_g_fea', 'l_g_range', 'l_g_gan', 'l_d_real', 'l_d_fake',
-                       'D_real', 'D_fake','D_logits_diff','psnr_val','D_update_ratio','LR_decrease']
+                       'D_real', 'D_fake','D_logits_diff','psnr_val','D_update_ratio','LR_decrease','Correctly_distinguished']
         self.log_dict = OrderedDict(zip(logs_2_keep, [[] for i in logs_2_keep]))
         self.debug = 'debug' in opt['path']['log']
         if self.is_train:
@@ -249,7 +249,9 @@ class SRRaGANModel(BaseModel):
                     if self.generator_step and self.opt['train']['D_valid_Steps_4_G_update'] > 0:
                         self.generator_step = len(self.log_dict['D_logits_diff']) >= self.opt['train']['D_valid_Steps_4_G_update'] and \
                                               all([val[1] > np.log(self.opt['train']['min_D_prob_ratio_4_G']) for val in
-                                              self.log_dict['D_logits_diff'][-self.opt['train']['D_valid_Steps_4_G_update']:]])
+                                              self.log_dict['D_logits_diff'][-self.opt['train']['D_valid_Steps_4_G_update']:]]) and \
+                                              all([val[1] > np.log(self.opt['train']['min_mean_D_correct']) for val in
+                                                   self.log_dict['Correctly_distinguished'][-self.opt['train']['D_valid_Steps_4_G_update']:]])
 
             if not VERIFY_D_USING_PAST_PERFORMANCE and self.generator_step:
                 self.generator_step = all([val > 0 for val in self.D_logits_diff_grad_step[-1]]) \
@@ -269,6 +271,7 @@ class SRRaGANModel(BaseModel):
                 self.log_dict['D_real'].append((self.gradient_step_num,np.mean(self.D_real_grad_step)))
                 self.log_dict['D_fake'].append((self.gradient_step_num,np.mean(self.D_fake_grad_step)))
                 self.log_dict['D_logits_diff'].append((self.gradient_step_num,np.mean(self.D_logits_diff_grad_step)))
+                self.log_dict['Correctly_distinguished'].append((self.gradient_step_num,np.mean([val0>0 for val1 in self.D_logits_diff_grad_step for val0 in val1])))
                 self.log_dict['D_update_ratio'].append((self.gradient_step_num,self.cur_D_update_ratio))
 
         # G step:
@@ -364,7 +367,7 @@ class SRRaGANModel(BaseModel):
         #The returned value is LR_too_low
         SLOPE_BASED = False
         LOSS_BASED = True
-        if len(self.log_dict['D_logits_diff'])==0 or self.log_dict['D_logits_diff'][0][0]>cur_step-self.opt['train']['steps_4_lr_std']:#Check after a minimal number of steps
+        if len(self.log_dict['D_logits_diff'])<2 * self.opt['train']['steps_4_lr_std'] or self.log_dict['D_logits_diff'][0][0]>cur_step-self.opt['train']['steps_4_lr_std']:#Check after a minimal number of steps
             return False
         if os.path.isfile(os.path.join(self.log_path, 'lr.npz')):#Allow enough steps between checks
             if cur_step - np.load(os.path.join(self.log_path, 'lr.npz'))['step_num'] <= 2 * self.opt['train']['steps_4_lr_std']:
@@ -428,7 +431,7 @@ class SRRaGANModel(BaseModel):
                 self.log_dict[key] = [tuple(val) for val in loaded_log[key]]
             else:
                 self.log_dict[key] = list(loaded_log[key])
-                if isinstance(self.log_dict[key][0][1],torch.Tensor):#Supporting old files where data was not converted from tensor - Causes slowness.
+                if len(self.log_dict[key])>0 and isinstance(self.log_dict[key][0][1],torch.Tensor):#Supporting old files where data was not converted from tensor - Causes slowness.
                     self.log_dict[key] = [[val[0],val[1].item()] for val in self.log_dict[key]]
             if max_step is not None:
                 self.log_dict[key] = [pair for pair in self.log_dict[key] if pair[0]<=max_step]

codes/train.py
@@ -123,83 +123,86 @@ def main():
 
             # validation
             if not_within_batch and (gradient_step_num) % opt['train']['val_freq'] == 0 and gradient_step_num>=opt['train']['D_init_iters']:
-                print('---------- validation -------------')
-                start_time = time.time()
-
-                if SAVE_IMAGE_COLLAGE:
-                    GT_image_collage = []
-                    cur_train_results = model.get_current_visuals(entire_batch=True)
-                    train_psnrs = [
-                        util.calculate_psnr(util.tensor2img(cur_train_results['SR'][im_num], out_type=np.float32) * 255,
-                                            util.tensor2img(cur_train_results['HR'][im_num], out_type=np.float32) * 255) for
-                        im_num in range(len(cur_train_results['SR']))]
-                    #Save latest training batch output:
-                    save_img_path = os.path.join(os.path.join(opt['path']['val_images']),
-                                                 '{:d}_Tr_PSNR{:.3f}.png'.format(gradient_step_num, np.mean(train_psnrs)))
-                    util.save_img(np.clip(np.concatenate(
-                        (np.concatenate(
-                            [util.tensor2img(cur_train_results['HR'][im_num], out_type=np.float32) * 255 for im_num in
-                             range(len(cur_train_results['SR']))],
-                            0), np.concatenate(
-                            [util.tensor2img(cur_train_results['SR'][im_num], out_type=np.float32) * 255 for im_num in
-                             range(len(cur_train_results['SR']))],
-                            0)), 1), 0, 255).astype(np.uint8), save_img_path)
-                Z_noise = [0]+([-1,1] if opt['network_G']['noise_input'] else [])
-                for cur_Z in Z_noise:
-                    avg_psnr = 0.0
-                    idx = 0
-                    image_collage = []
-                    for val_data in tqdm.tqdm(val_loader):
-                        if idx%val_images_collage_rows==0:  image_collage.append([]);   GT_image_collage.append([])
-                        idx += 1
-                        img_name = os.path.splitext(os.path.basename(val_data['LR_path'][0]))[0]
-                        img_dir = os.path.join(opt['path']['val_images'], img_name)
-                        util.mkdir(img_dir)
-                        val_data['Z'] = cur_Z
-                        model.feed_data(val_data)
-                        model.test()
-
-                        visuals = model.get_current_visuals()
-                        sr_img = util.tensor2img(visuals['SR'],out_type=np.float32)  # float32
-                        gt_img = util.tensor2img(visuals['HR'],out_type=np.float32)  # float32
-
-                        # calculate PSNR
-                        crop_size = opt['scale']
-                        gt_img *= 255.
-                        sr_img *= 255.
-                        # cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                        # cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
-                        # avg_psnr += util.calculate_psnr(cropped_sr_img, cropped_gt_img)
-                        avg_psnr += util.calculate_psnr(sr_img, gt_img)
-
-                        if SAVE_IMAGE_COLLAGE:
-                            margins2crop = ((np.array(sr_img.shape[:2])-per_image_saved_patch)/2).astype(np.int32)
-                            image_collage[-1].append(np.clip(sr_img[margins2crop[0]:-margins2crop[0],margins2crop[1]:-margins2crop[1],:],0,255).astype(np.uint8))
-                            if save_GT_HR:#Save GT HR images
-                                GT_image_collage[-1].append(np.clip(gt_img[margins2crop[0]:-margins2crop[0],margins2crop[1]:-margins2crop[1],:],0,255).astype(np.uint8))
-                        else:
-                            # Save SR images for reference
-                            save_img_path = os.path.join(img_dir, '{:s}_{:d}.png'.format(img_name, gradient_step_num))
-                            util.save_img(np.clip(sr_img,0,255).astype(np.uint8), save_img_path)
-
-                    avg_psnr = avg_psnr / idx
+                print_rlt = OrderedDict()
+                if model.generator_changed:
+                    print('---------- validation -------------')
+                    start_time = time.time()
                     if SAVE_IMAGE_COLLAGE:
-                        save_img_path = os.path.join(os.path.join(opt['path']['val_images']), '{:d}_{}PSNR{:.3f}.png'.format(gradient_step_num,('Z'+str(cur_Z)) if opt['network_G']['noise_input'] else '',avg_psnr))
-                        util.save_img(np.concatenate([np.concatenate(col,0) for col in image_collage],1), save_img_path)
-                        if save_GT_HR:  # Save GT HR images
-                            util.save_img(np.concatenate([np.concatenate(col, 0) for col in GT_image_collage], 1),
-                                os.path.join(os.path.join(opt['path']['val_images']), 'GT_HR.png'))
-                            save_GT_HR = False
+                        GT_image_collage = []
+                        cur_train_results = model.get_current_visuals(entire_batch=True)
+                        train_psnrs = [
+                            util.calculate_psnr(util.tensor2img(cur_train_results['SR'][im_num], out_type=np.float32) * 255,
+                                                util.tensor2img(cur_train_results['HR'][im_num], out_type=np.float32) * 255) for
+                            im_num in range(len(cur_train_results['SR']))]
+                        #Save latest training batch output:
+                        save_img_path = os.path.join(os.path.join(opt['path']['val_images']),
+                                                     '{:d}_Tr_PSNR{:.3f}.png'.format(gradient_step_num, np.mean(train_psnrs)))
+                        util.save_img(np.clip(np.concatenate(
+                            (np.concatenate(
+                                [util.tensor2img(cur_train_results['HR'][im_num], out_type=np.float32) * 255 for im_num in
+                                 range(len(cur_train_results['SR']))],
+                                0), np.concatenate(
+                                [util.tensor2img(cur_train_results['SR'][im_num], out_type=np.float32) * 255 for im_num in
+                                 range(len(cur_train_results['SR']))],
+                                0)), 1), 0, 255).astype(np.uint8), save_img_path)
+                    Z_noise = [0]+([-1,1] if opt['network_G']['noise_input'] else [])
+                    for cur_Z in Z_noise:
+                        avg_psnr = 0.0
+                        idx = 0
+                        image_collage = []
+                        for val_data in tqdm.tqdm(val_loader):
+                            if idx%val_images_collage_rows==0:  image_collage.append([]);   GT_image_collage.append([])
+                            idx += 1
+                            img_name = os.path.splitext(os.path.basename(val_data['LR_path'][0]))[0]
+                            img_dir = os.path.join(opt['path']['val_images'], img_name)
+                            util.mkdir(img_dir)
+                            val_data['Z'] = cur_Z
+                            model.feed_data(val_data)
+                            model.test()
+
+                            visuals = model.get_current_visuals()
+                            sr_img = util.tensor2img(visuals['SR'],out_type=np.float32)  # float32
+                            gt_img = util.tensor2img(visuals['HR'],out_type=np.float32)  # float32
+
+                            # calculate PSNR
+                            crop_size = opt['scale']
+                            gt_img *= 255.
+                            sr_img *= 255.
+                            # cropped_sr_img = sr_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                            # cropped_gt_img = gt_img[crop_size:-crop_size, crop_size:-crop_size, :]
+                            # avg_psnr += util.calculate_psnr(cropped_sr_img, cropped_gt_img)
+                            avg_psnr += util.calculate_psnr(sr_img, gt_img)
+
+                            if SAVE_IMAGE_COLLAGE:
+                                margins2crop = ((np.array(sr_img.shape[:2])-per_image_saved_patch)/2).astype(np.int32)
+                                image_collage[-1].append(np.clip(sr_img[margins2crop[0]:-margins2crop[0],margins2crop[1]:-margins2crop[1],:],0,255).astype(np.uint8))
+                                if save_GT_HR:#Save GT HR images
+                                    GT_image_collage[-1].append(np.clip(gt_img[margins2crop[0]:-margins2crop[0],margins2crop[1]:-margins2crop[1],:],0,255).astype(np.uint8))
+                            else:
+                                # Save SR images for reference
+                                save_img_path = os.path.join(img_dir, '{:s}_{:d}.png'.format(img_name, gradient_step_num))
+                                util.save_img(np.clip(sr_img,0,255).astype(np.uint8), save_img_path)
+
+                        avg_psnr = avg_psnr / idx
+                        if SAVE_IMAGE_COLLAGE:
+                            save_img_path = os.path.join(os.path.join(opt['path']['val_images']), '{:d}_{}PSNR{:.3f}.png'.format(gradient_step_num,('Z'+str(cur_Z)) if opt['network_G']['noise_input'] else '',avg_psnr))
+                            util.save_img(np.concatenate([np.concatenate(col,0) for col in image_collage],1), save_img_path)
+                            if save_GT_HR:  # Save GT HR images
+                                util.save_img(np.concatenate([np.concatenate(col, 0) for col in GT_image_collage], 1),
+                                    os.path.join(os.path.join(opt['path']['val_images']), 'GT_HR.png'))
+                                save_GT_HR = False
+                    model.log_dict['psnr_val'].append((gradient_step_num,avg_psnr))
+                    print_rlt['psnr'] = avg_psnr
+                else:
+                    print('Skipping validation because generator is unchanged')
                 time_elapsed = time.time() - start_time
                 # Save to log
-                print_rlt = OrderedDict()
                 print_rlt['model'] = opt['model']
                 print_rlt['epoch'] = epoch
                 print_rlt['iters'] = gradient_step_num
                 print_rlt['time'] = time_elapsed
-                print_rlt['psnr'] = avg_psnr
-                model.log_dict['psnr_val'].append((gradient_step_num,avg_psnr))
                 model.display_log_figure()
+                model.generator_changed = False
                 logger.print_format_results('val', print_rlt)
                 print('-----------------------------------')
 