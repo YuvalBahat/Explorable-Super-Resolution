-Some small bug fixes -Displaying average training time instead of arbitrary iteration time.

codes/GUI.py
@@ -1337,11 +1337,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.show()
 
     def Manipulate_HSV(self,channel,increase):
+        STEP_SIZE = 0.1 # 0.01
         self.canvas.scribble_mode_entry_operations()
         existing_scribble_im = qimage2ndarray.rgb_view(self.canvas.pixmap().toImage())
         HSV_image = rgb2hsv(existing_scribble_im)
         multiplier = 1.01 if increase else 0.99
-        adder = 0.01 if increase else -0.01
+        adder = STEP_SIZE if increase else -1*STEP_SIZE
         if channel=='H':
             # HSV_image[:,:,0] = HSV_image[:,:,0]*(1-self.canvas.HR_mask_display_size)+np.mod(self.canvas.HR_mask_display_size*multiplier*HSV_image[:,:,0],1)
             HSV_image[:,:,0] = np.mod(HSV_image[:,:,0]+self.canvas.HR_mask_display_size*adder,1)
@@ -1797,7 +1798,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                     self.canvas.Z_optimizer_logger.append(Logger(self.canvas.opt,tb_logger_suffix='_%s%s'%(objective,'_%d'%(i) if self.multiple_inits else '')))
             if self.JPEG_GUI:
                 data['Comp'] = data['LR']
-                data['QF'] = self.canvas.SR_model.jpeg_extractor.QF
+                data['QF'] = self.canvas.SR_model.jpeg_extractor.QF.view(1)
                 data['chroma_input'] = self.chroma_input
             self.canvas.Z_optimizer = Z_optimizer(objective=objective,Z_size=[val*self.canvas.SR_model.Z_size_factor for val in data['LR'].size()[2:]],model=self.canvas.SR_model,
                 Z_range=self.max_SVD_Lambda,data=data,initial_LR=self.canvas.Z_optimizer_initial_LR,loggers=self.canvas.Z_optimizer_logger,max_iters=self.iters_per_round,

codes/JPEG_module/JPEG.py
@@ -69,7 +69,7 @@ class JPEG(nn.Module):
     def Set_Q_Table(self,QF_or_table,QF=True):
         if QF:
             self.QF = QF_or_table
-            condition = (QF_or_table < 50).to(self.device).type(torch.LongTensor)
+            condition = (QF_or_table < 50).to(self.device).type(self.QF.type())
             self.factor = (condition*(5000 / QF_or_table) + (1-condition)*(200 - 2 * QF_or_table))
             self.factor = self.factor.view([-1,1,1,1,1]+([1] if self.chroma_mode else [])).type(self.synthetic_Q_table.dtype).to(self.device)
             self.Q_table = torch.clamp((self.factor * self.synthetic_Q_table).round(),0,255)

codes/models/DecompCNN_model.py
@@ -324,7 +324,7 @@ class DecompCNNModel(BaseModel):
         if self.DCT_discriminator:
             self.jpeg_non_quantized_compressor.Set_Q_Table(self.QF)
         if self.latent_input is not None:
-            input_size = np.array(data['Uncomp'].size()) if 'Uncomp' in data.keys() else 8*np.array(data['Comp'].size())
+            input_size = np.array(data['Uncomp'].size()) if 'Uncomp' in data.keys() else [1,1,8,8]*np.array(data['Comp'].size())
             DCT_dims = list(input_size[2:]//8)
             # DCT_dims = list(np.array(self.var_Comp.size())[2:]) # Spatial dimensions of the latent channel correspond to those of the Y channel DCT coefficients.
             # Z is downsampled for the chroma channels generator

codes/train_JPEG.py
@@ -85,7 +85,7 @@ def main():
     logger = Logger(opt)
     # Save validation set results as image collage:
     SAVE_IMAGE_COLLAGE = True
-    start_time = time.time()
+    start_time,start_time_gradient_step = time.time(),model.step // max_accumulation_steps
     save_GT_Uncomp = True
     lr_too_low = False
     print('---------- Start training -------------')
@@ -115,8 +115,8 @@ def main():
             if model.step > total_iters:
                 break
 
-            time_elapsed = time.time() - start_time
-            if not_within_batch:    start_time = time.time()
+            # time_elapsed = time.time() - start_time
+            # if not_within_batch:    start_time = time.time()
             # log
             if model.gradient_step_num % opt['logger']['print_freq'] == 0 and not_within_batch:
                 logs = model.get_current_log()
@@ -124,7 +124,9 @@ def main():
                 print_rlt['model'] = opt['model']
                 print_rlt['epoch'] = epoch
                 print_rlt['iters'] = model.gradient_step_num
-                print_rlt['time'] = time_elapsed
+                # time_elapsed = time.time() - start_time
+                print_rlt['time'] = (time.time() - start_time)/np.maximum(1,model.gradient_step_num-start_time_gradient_step)
+                start_time, start_time_gradient_step = time.time(), model.gradient_step_num
                 for k, v in logs.items():
                     print_rlt[k] = v
                 print_rlt['lr'] = model.get_current_learning_rate()
@@ -160,12 +162,13 @@ def main():
                     model.log_dict['psnr_val'].append((model.gradient_step_num,print_rlt['psnr']))
                 else:
                     print('Skipping validation because generator is unchanged')
-                time_elapsed = time.time() - start_time
+                # time_elapsed = time.time() - start_time
                 # Save to log
                 print_rlt['model'] = opt['model']
                 print_rlt['epoch'] = epoch
                 print_rlt['iters'] = model.gradient_step_num
-                print_rlt['time'] = time_elapsed
+                # print_rlt['time'] = time_elapsed
+                print_rlt['time'] = (time.time() - start_time)/np.maximum(1,model.gradient_step_num-start_time_gradient_step)
                 # model.display_log_figure()
                 # model.generator_changed = False
                 logger.print_format_results('val', print_rlt,keys_ignore_list=['avg_est_err'])