Many changes: -Adding parameter n_k that allows using 1x1 convolutions in deeper layers of D -Fixing a mistake in hinge-loss, where for patch discriminator the average output per image was clipped instead of the per-patch score. -When performing evaluation during training, adding functions in base_model to allow using a running average over model parameters in the X number of update steps.

codes/models/base_model.py
@@ -5,8 +5,10 @@ import CEM.CEMnet as CEMnet
 import numpy as np
 import collections
 import matplotlib.pyplot as plt
-from torch.nn import Upsample
-from utils.util import compute_RF_numerical
+import re
+import copy
+# from torch.nn import Upsample
+# from utils.util import compute_RF_numerical
 
 class BaseModel():
     def __init__(self, opt):
@@ -138,6 +140,45 @@ class BaseModel():
         # network.load_state_dict(loaded_state_dict, strict=(strict and not self.opt['network_G']['CEM_arch']))
         loaded_state_dict = self.process_loaded_state_dict(loaded_state_dict=loaded_state_dict,current_state_dict=network.state_dict())
         network.load_state_dict(loaded_state_dict, strict=strict)
+
+    # def average_across_model_snapshots(self,apply):
+    #     #     Used before performing evaluation during training. When apply=True, saving the current generator weights, and loading and averaging across latest svaed weights. When apply=False, restoring current weights.
+    #     if apply:
+    #         self.temp_stored_G_weights = copy.deepcopy(self.netG.state_dict())
+    #         averaged_dicts = []
+    #         for snapshot in os.listdir(self.save_dir):
+    #             if '_G.pth' in snapshot and int(re.search('(\d)+(?=_G)',snapshot).group(0))>self.gradient_step_num-self.opt['train']['val_save_freq']:
+    #                 averaged_dicts.append(os.path.join(self.save_dir,snapshot))
+    #         print('Evaluating by averaging weights over %d latest snapshots'%(len(averaged_dicts)+1))
+    #         averaged_state_dict = self.netG.state_dict()
+    #         for i,snapshot in enumerate(averaged_dicts):
+    #             loaded_snapshot = torch.load(snapshot)
+    #             for key in averaged_state_dict:
+    #                 averaged_state_dict[key] = (i+1)/(i+2)*averaged_state_dict[key]+1/(i+2)*loaded_snapshot['model_state_dict'][key.replace('module.','')].cuda()
+    #         self.netG.load_state_dict(averaged_state_dict)
+    #     else:
+    #         self.netG.load_state_dict(self.temp_stored_G_weights)
+
+    def update_running_avg(self):
+        translated_step_num = [self.gradient_step_num+i for i in range(min([self.opt['train']['val_running_avg_steps'],self.gradient_step_num+1]))]
+        steps_before_eval = [(v%self.opt['train']['val_freq'])==0 for v in translated_step_num]
+        if any(steps_before_eval):
+            cur_state_dic = self.netG.state_dict()
+            if steps_before_eval[-1]: #If this is the first step of calculating running average:
+                self.weights_averaging_counter = 1
+                self.running_avg_weights = copy.deepcopy(cur_state_dic)
+            else:
+                self.weights_averaging_counter += 1
+                for key in cur_state_dic:
+                    self.running_avg_weights[key] = (self.weights_averaging_counter-1)/self.weights_averaging_counter*self.running_avg_weights[key]+\
+                        1/self.weights_averaging_counter*cur_state_dic[key]
+
+    def toggle_running_avg_weight(self,on):
+        if on:
+            self.temp_saved_weights = copy.deepcopy(self.netG.state_dict())
+            self.netG.load_state_dict(self.running_avg_weights)
+        else:
+            self.netG.load_state_dict(self.temp_saved_weights)
 
     def process_loaded_state_dict(self,loaded_state_dict,current_state_dict):
         SPECTRAL_NORMALIZATIONFIX_PATCH = False

codes/models/modules/architecture.py
@@ -78,7 +78,6 @@ class SRResNet(nn.Module):
         resnet_blocks = [B.ResNetBlock(nf, nf, nf, norm_type=norm_type, act_type=act_type,\
             mode=mode, res_scale=res_scale) for _ in range(nb)]
         LR_conv = B.conv_block(nf, nf, kernel_size=3, norm_type=norm_type, act_type=None, mode=mode)
-
         if upsample_mode == 'upconv':
             upsample_block = B.upconv_blcok
         elif upsample_mode == 'pixelshuffle':
@@ -107,12 +106,16 @@ class Flatten(nn.Module):
         return input.view(input.size(0), -1)
 
 class DnCNN(nn.Module):
-    def __init__(self, n_channels, depth, kernel_size = 3, in_nc=64, out_nc=64, norm_type='batch', act_type='leakyrelu',
+    def __init__(self, n_channels, depth, kernel_size = 3, in_nc=64, out_nc=64,num_kerneled_layers=None, norm_type='batch', act_type='leakyrelu',
                  latent_input=None,num_latent_channels=None,discriminator=False,expected_input_size=None,chroma_generator=False,spectral_norm=False,pooling_no_FC=False):
         super(DnCNN, self).__init__()
         # assert in_nc in [64,128] and out_nc==64,'Currently only supporting 64 DCT channels'
         assert act_type=='leakyrelu'
         assert norm_type in ['batch','instance','layer',None]
+        if num_kerneled_layers is None:
+            num_kerneled_layers = 1*depth
+        else:
+            assert num_kerneled_layers<=depth
         # self.average_err_collection_counter = 0
         # self.average_abs_err_estimates = np.zeros([8,8])
         self.discriminator_net = discriminator
@@ -120,7 +123,8 @@ class DnCNN(nn.Module):
             # Ideally I should not use padding for the discriminator model. I do use padding in the first layers if the input size is too small,
             # so that the dimension of the fully connected layer's input would be at least MIN_DCT_DIMS_4_D x MIN_DCT_DIMS_4_D
             MIN_DCT_DIMS_4_D = 5
-            num_padded_layers = max(0,depth-int(np.floor((expected_input_size-MIN_DCT_DIMS_4_D)/(kernel_size-1))))
+            num_padded_layers = max(0,num_kerneled_layers-int(np.floor((expected_input_size-MIN_DCT_DIMS_4_D)/(kernel_size-1))))
+            assert num_padded_layers==0 or num_kerneled_layers==depth,'This specific case is unsupported yet. See if I even want this padding for a patch-D, which I assume is the case here.'
             layer_num = 0
             self.pooling_no_FC = pooling_no_FC
         else:
@@ -138,17 +142,23 @@ class DnCNN(nn.Module):
             self.num_latent_channels = 0
 
         layers = []
-        if self.discriminator_net and layer_num>=num_padded_layers:
-            expected_input_size -= (kernel_size - 1)
-            padding = 0
+        if self.discriminator_net:
+            if layer_num>=num_padded_layers:
+                expected_input_size -= (kernel_size - 1)
+                padding = 0
+            if layer_num>=num_kerneled_layers:
+                kernel_size = 1
         layers.append(nn.Conv2d(in_channels=in_nc+self.num_latent_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding,bias=True))
         if spectral_norm:
             layers[-1] = SN.spectral_norm(layers[-1])
         layers.append(nn.ReLU(inplace=True))
         for layer_num in range(1,depth - 2+1):
-            if self.discriminator_net and layer_num>=num_padded_layers:
-                expected_input_size -= (kernel_size-1)
-                padding = 0
+            if self.discriminator_net:
+                if layer_num>=num_padded_layers:
+                    expected_input_size -= (kernel_size-1)
+                    padding = 0
+                if layer_num >= num_kerneled_layers:
+                    kernel_size = 1
             layers.append(nn.Conv2d(in_channels=n_channels+self.num_latent_channels*(self.latent_input=='all_layers'), out_channels=n_channels, kernel_size=kernel_size, padding=padding,bias=False))
             if spectral_norm:
                 layers[-1] = SN.spectral_norm(layers[-1])
@@ -160,9 +170,12 @@ class DnCNN(nn.Module):
                 layers.append(nn.InstanceNorm2d(n_channels))
             layers.append(nn.LeakyReLU(inplace=True))
         layer_num += 1
-        if self.discriminator_net and layer_num >= num_padded_layers:
-            expected_input_size -= (kernel_size - 1)
-            padding = 0
+        if self.discriminator_net:
+            if layer_num >= num_padded_layers:
+                expected_input_size -= (kernel_size - 1)
+                padding = 0
+            if layer_num >= num_kerneled_layers:
+                kernel_size = 1
         layers.append(nn.Conv2d(in_channels=n_channels+self.num_latent_channels*(self.latent_input=='all_layers'),
             out_channels=1 if (self.discriminator_net and self.pooling_no_FC) else out_nc, kernel_size=kernel_size, padding=padding,
                                 bias=self.discriminator_net and self.pooling_no_FC)) #When using a fully convolutional D (when pooling_no_FC), allowing bias in the final layer.

codes/models/modules/loss.py
@@ -239,8 +239,10 @@ class GANLoss(nn.Module):
             return torch.empty_like(input).fill_(self.fake_label_val)
 
     def forward(self, input, target_is_real,hinge_threshold=None):
-        # CLIPPING_AMP = 1.0
+        LEGACY_HINGE_CLIPPING_BUG = False
         if hinge_threshold is not None:
+            if LEGACY_HINGE_CLIPPING_BUG:
+                input = torch.mean(input,dim=[d for d in range(1,input.dim())])
             input = torch.clamp_max(input,hinge_threshold) if target_is_real else torch.clamp_min(input,-1*hinge_threshold)
         target_label = self.get_target_label(input, target_is_real)
         loss = self.loss(input, target_label)

codes/train_JPEG.py
@@ -95,6 +95,7 @@ def main():
         for i, train_data in enumerate(train_loader):
             model.gradient_step_num = model.step // (max_accumulation_steps*(2 if model.D_exists and model.opt['train']['G_Dbatch_separation']=='SeparateBatch' else 1))
             not_within_batch = model.step % max_accumulation_steps == (max_accumulation_steps - 1)
+            if not_within_batch:    model.update_running_avg()
             saving_step = ((time.time()-last_saving_time)>60*opt['logger']['save_checkpoint_freq']) and not_within_batch
             if saving_step:
                 last_saving_time = time.time()
@@ -139,23 +140,16 @@ def main():
                 if model.generator_changed:
                     print('---------- validation -------------')
                     start_time = time.time()
-                    if False and SAVE_IMAGE_COLLAGE and model.gradient_step_num%opt['train']['val_save_freq'] == 0: #Saving training images:
-                        # GT_image_collage,quantized_image_collage = [],[]
-                        cur_train_results = model.get_current_visuals(entire_batch=True)
-                        train_psnrs = [util.calculate_psnr(util.tensor2img(cur_train_results['Decomp'][im_num], out_type=np.uint8,min_max=[0,255]),
-                            util.tensor2img(cur_train_results['Uncomp'][im_num], out_type=np.uint8,min_max=[0,255])) for im_num in range(len(cur_train_results['Decomp']))]
-                        #Save latest training batch output:
-                        save_img_path = os.path.join(os.path.join(opt['path']['val_images']),
-                                                     '{:d}_Tr_PSNR{:.3f}.png'.format(model.gradient_step_num, np.mean(train_psnrs)))
-                        util.save_img(np.clip(np.concatenate((np.concatenate([util.tensor2img(cur_train_results['Uncomp'][im_num], out_type=np.uint8,min_max=[0,255]) for im_num in
-                                 range(len(cur_train_results['Decomp']))],0), np.concatenate(
-                                [util.tensor2img(cur_train_results['Decomp'][im_num], out_type=np.uint8,min_max=[0,255]) for im_num in range(len(cur_train_results['Decomp']))],
-                                0)), 1), 0, 255).astype(np.uint8), save_img_path)
+                    save_images = ((model.gradient_step_num) % opt['train']['val_save_freq'] == 0) or save_GT_Uncomp
                     Z_latent = [0]+([-0.5,0.5] if (opt['network_G']['latent_input'] and opt['network_G']['latent_channels']>0) else [])
                     print_rlt['psnr'] = 0
+                    model.toggle_running_avg_weight(True)
+                    # if save_images: model.average_across_model_snapshots(apply=True)
                     for cur_Z in Z_latent:
                         model.perform_validation(data_loader=val_loader,cur_Z=cur_Z,print_rlt=print_rlt,GT_and_quantized=save_GT_Uncomp,
-                                                 save_images=((model.gradient_step_num) % opt['train']['val_save_freq'] == 0) or save_GT_Uncomp)
+                                                 save_images=save_images)
+                    model.toggle_running_avg_weight(False)
+                    # if save_images: model.average_across_model_snapshots(apply=False)
                     if save_GT_Uncomp:  # Save GT Uncomp images
                         save_GT_Uncomp = False
                     print_rlt['psnr'] /= len(Z_latent)